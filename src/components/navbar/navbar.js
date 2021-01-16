18d222741b61012e0892f5037e73e2ffaab8048d
alihussain575 <you@example.com>
2021-01-17 01:30:24 +0500

    updated navbar

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,14 +1,24 @@
 import React  from 'react'
+import './navbar.css'
+import Aux from '../../hoc/Auxiliary'
 
 function navbar() {
 
     return (
-        <div>
-            <h1 className="tite">Freelancers.co</h1> 
-            <a href="#">Home</a>
-            <button>login</button>
-            <button>sign up</button>
-        </div>
+        <Aux>
+            <div className="nav">
+                <h2 className="title">Freelancers<span>.co</span></h2> 
+                <div className="items">
+                    <a href="">Home</a>
+                    <a href="#">Hire</a>
+                    <a href="#">Get Hired</a>
+                    <a href="#">Articles</a>
+                    <input type="text" placeholder="Search"/>
+                    <button className="search">Search</button>
+                </div>
+            </div>
+            <div className=""></div>
+        </Aux>
     )
 }
 
