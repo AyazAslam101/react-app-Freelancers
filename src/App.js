120592dcdf8c592e2a65e20e1522ed90ec9b6d47
alihussain575 <you@example.com>
2021-01-20 03:01:15 +0500

    new files

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from 'react'
+import React,{useState} from 'react'
 import './App.css';
 import Navbar from './components/navbar/navbar'
 // import Freelancer from "./components/freelancers/freelancers"
 
 function App() {
+  const [searchBarRender , setSearchBarRender] = useState({
+    showSearch : true
+})
+// const showSearchHandler = ()=>{
+//   let show = searchBarRender.showSeacrh
+//   setSearchBarRender({
+//     showSearch: !show
+//   })
+// }
+
   return (
     <div className="App">
-      <Navbar />
+      <Navbar search={searchBarRender} />
     </div>
   );
 }
