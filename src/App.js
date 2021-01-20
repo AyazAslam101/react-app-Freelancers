87d7bf8dbb24754b1518d9215af490f4480246e9
Ayaz Aslam <70416532+AyazAslam101@users.noreply.github.com>
2021-01-21 03:58:34 +0500

    SignUP Page

--- src/App.js
+++ src/App.js
@@ -1,27 +1,27 @@
-import React,{useState} from 'react'
-import {BrowserRouter as Router , Switch , Route} from 'react-router-dom'
+import React from 'react'
+// import {BrowserRouter as Router , Switch , Route} from 'react-router-dom'
 import './App.css';
-import Navbar from './components/navbar/navbar'
-import FreelancerLogin from "./components/freelancerForm/freelancerLoginForm/freelancerLoginFrom"
-// import Freelancer from "./components/freelancers/freelancers"
-import HomePage from './components/homePage/homePage'
-import hirePage from './components/hirePage/hirePage';
-import getHirePage from './components/getHirePage/getHirePage';
-
+// import Navbar from './components/navbar/navbar'
+// import FreelancerLogin from "./components/freelancerForm/freelancerLoginForm/freelancerLoginFrom"
+// // import Freelancer from "./components/freelancers/freelancers"
+// import HomePage from './components/homePage/homePage';
+// import hirePage from './components/hirePage/hirePage';
+// import getHirePage from './components/getHirePage/getHirePage';
+import SignUpModal from './components/signUpModal/signUpModal'
 function App() {
-  const [searchBarRender , setSearchBarRender] = useState({
-    showSearch : false
-})
-const showSearchHandler = ()=>{
-  let show = searchBarRender.showSeacrh
-  setSearchBarRender({
-    showSearch: !show
-  })
-}
+//   const [searchBarRender , setSearchBarRender] = useState({
+//     showSearch : false
+// })
+// const showSearchHandler = ()=>{
+//   let show = searchBarRender.showSeacrh
+//   setSearchBarRender({
+//     showSearch: !show
+//   })
+// }
 
   return (
     <div className="App">
-      <Router>
+      {/* <Router>
         <Navbar search={searchBarRender} />
         <Switch>
           <Route path="/" exact component={FreelancerLogin} ><FreelancerLogin 
@@ -30,7 +30,8 @@ const showSearchHandler = ()=>{
           <Route path="/hire" exact component={hirePage}/>
           <Route path="/getHire" exact component={getHirePage}/>
         </Switch>
-      </Router>
+      </Router> */}
+          <SignUpModal/>
     </div>
   );
 }
---
+++ src/components/signUpModal/signUpModal.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import SignUpForm from "../freelancerForm/freelancersignup/freelancerSignup"
+
+function signUpModal() {
+    return (
+        <div>
+            <SignUpForm/>
+        </div>
+    )
+}
+
+export default signUpModal
