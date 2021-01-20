1df985b4d768a17f8ef31eb408f71d94fcec820c
alihussain575 <you@example.com>
2021-01-21 04:45:26 +0500

    new

--- src/App.js
+++ src/App.js
@@ -1,38 +1,55 @@
-import React from 'react'
-// import {BrowserRouter as Router , Switch , Route} from 'react-router-dom'
+import React, {useState} from 'react'
+import {BrowserRouter as Router , Switch , Route} from 'react-router-dom'
 import './App.css';
-// import Navbar from './components/navbar/navbar'
-// import FreelancerLogin from "./components/freelancerForm/freelancerLoginForm/freelancerLoginFrom"
-// // import Freelancer from "./components/freelancers/freelancers"
-// import HomePage from './components/homePage/homePage';
-// import hirePage from './components/hirePage/hirePage';
-// import getHirePage from './components/getHirePage/getHirePage';
-import SignUpModal from './components/signUpModal/signUpModal'
+import Navbar from './components/navbar/navbar'
+import FreelancerLogin from "./components/freelancerForm/freelancerLoginForm/freelancerLoginFrom"
+// import Freelancer from "./components/freelancers/freelancers"
+import HomePage from './components/homePage/homePage';
+import hirePage from './components/hirePage/hirePage';
+import getHirePage from './components/getHirePage/getHirePage';
 function App() {
 
-//   const [searchBarRender , setSearchBarRender] = useState({
-//     showSearch : false
-// })
-// const showSearchHandler = ()=>{
-//   let show = searchBarRender.showSeacrh
-//   setSearchBarRender({
-//     showSearch: !show
-//   })
-// }
+  const [searchBarRender , setSearchBarRender] = useState({
+    showSearch : false
+})
+const [isModalVisible, setIsModalVisible] = useState(false);
+
+const showModal = () => {
+  setIsModalVisible(true);
+};
+
+const handleOk = () => {
+  setIsModalVisible(false);
+};
+
+const handleCancel = () => {
+  setIsModalVisible(false);
+};
+
+const showSearchHandler = ()=>{
+  let show = searchBarRender.showSeacrh
+  setSearchBarRender({
+    showSearch: !show
+  })
+}
 
   return (
     <div className="App">
-      {/* <Router>
+      <Router>
         <Navbar search={searchBarRender} />
         <Switch>
           <Route path="/" exact component={FreelancerLogin} ><FreelancerLogin 
-          showSearch={showSearchHandler}/></Route>
+          showSearch={showSearchHandler}
+          showModal={showModal}
+          handleOk={handleOk}
+          handleCancel={handleCancel}
+          isModalVisible={isModalVisible}/></Route>
           <Route path="/home" exact component={HomePage}/>
           <Route path="/hire" exact component={hirePage}/>
           <Route path="/getHire" exact component={getHirePage}/>
         </Switch>
-      </Router> */}
-          <SignUpModal/>
+      </Router>
+          
     </div>
   );
 }
--- src/components/freelancerForm/freelancerLoginForm/freelancerLoginFrom.js
+++ src/components/freelancerForm/freelancerLoginForm/freelancerLoginFrom.js
@@ -1,6 +1,8 @@
 import React,{useState} from 'react'
 import {Button} from 'antd'
 import './freelancerLogin.css'
+import SignUpModal from '../../signUpModal/signUpModal'
+import {Modal} from 'antd'
 import {useHistory } from 'react-router-dom'
 function FreelancerLoginFrom(props) {
     const history = useHistory()
@@ -22,13 +24,18 @@ function FreelancerLoginFrom(props) {
         <input  className="password-information" type="Password" placeholder="password"></input>
         </div>
         <div className="login-button">
-        <button className="button-login" onClick={goToHomePage} 
+        <button className="button-login" 
+        onClick={goToHomePage} 
         >Login in</button>
         </div>
         <div className="a-tag"><Button>Forgotten Password?</Button></div>
         <div className="line">
         </div>
-        <button className="acc-button">Create New Account</button>
+        <button className="acc-button"
+        onClick={props.showModal}>Create New Account</button>
+            <Modal title="Basic Modal" visible={props.isModalVisible} onOk={props.handleOk} onCancel={props.handleCancel}>
+                <SignUpModal/>
+             </Modal>
         </div>
     )
 }
