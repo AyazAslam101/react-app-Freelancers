dce749ff2965d3d46ad7bf256f9668f6c2f16dfd
Ayaz Aslam <70416532+AyazAslam101@users.noreply.github.com>
2021-01-26 23:10:06 +0500

    profile

--- src/components/freelancerForm/freelancerLoginForm/freelancerLoginFrom.js
+++ src/components/freelancerForm/freelancerLoginForm/freelancerLoginFrom.js
@@ -1,106 +1,53 @@
 import React,{useState} from 'react'
 import { Modal, Button } from 'antd';
 import './freelancerLogin.css'
-import SignUpModal from '../../signUpModal/signUpModal'
-import {Modal , message , Form} from 'antd'
-import {useSelector} from 'react-redux'
 import {useHistory } from 'react-router-dom'
 import SignUpModal from "../../signUpModal/signUpModal"
 function FreelancerLoginFrom(props) {
-
-    let data = useSelector(state=>state.userDetails)
-
-    let users = data.users
-
-    const addUser = (emailData)=>{
-        const filter = data.users.filter((element)=> element.email === emailData.email )
-        if(filter.lenght > 0){
-            message.warn("email taken")
-            return false
-        }else{
-            setloginData({...loginData , users })
-        }
-    }
-
-    // const success = () => {
-    //     message
-    //       .loading('Action in progress..', 2.5)
-    //       .then(() => message.success('Loading finished', 2.5))
-    //       .then(() => message.info('Loading finished is finished', 2.5));
-    //   };
-    
     const history = useHistory()
     const [homePage] = useState({
         routerpage:true
     })
-    const goToHomePage =(e)=>{
-        e.preventDefault()
-        console.log(data.users);
-        let result = data.users.find((element) => {
-            return (element.email === loginData.email && element.password === loginData.password)
-        })
-        console.log(result)
-        if(result){
+    const goToHomePage =()=>{
+        if(homePage.routerpage === true){
             history.push('/home')
-            message
-            .loading('Action in progress..', 2.5)
-            .then(() => message.success('Loading finished', 2.5));
-        }else{
-            message
-            .loading('Action in progress..', 2.5)
-            .then(()=> message.warning('Email or Password is wrong' , 2.5))
         }
     }
+
+    const [isModalVisible, setIsModalVisible] = useState(false);
+
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
     
     return (
-        <Aux>
-            <div className="login-form">
-            <Form onSubmit={goToHomePage} >
-                <h4>LOGIN FORM</h4>
-            <div className="login-information">
-                <Form.Item
-                name="Email"
-                rules={[
-                    {
-                      required : true,
-                      message : 'Please input your Email!'
-                    }
-                  ]}>
-                    <Input className="email-information" type={Text} placeholder="Email Adress or phone number" id="email" onChange={onChangeHandler}></Input>
-                </Form.Item>
-                <Form.Item
-                name="Password"
-                rules={[
-                    {
-                      required : true,
-                      message : 'Please input valid Password!'
-                    }
-                  ]}>
-                    <Input  className="password-information" type="Password" placeholder="password" id="password"onChange={onChangeHandler}></Input>
-                </Form.Item>
-            </div>
-            <div className="login-button">
-            <button type='submit' className="button-login" 
-            onSubmit={goToHomePage} 
-            // onClick={success}
-            >Login in</button>
-            </div>
-            <div className="a-tag"><Button>Forgotten Password?</Button></div>
-            <div className="line">
-            </div>
-            </Form>
-                <div>
-                <button className="acc-button"
-                onClick={props.showModal}>Create New Account</button>
-                </div>
-            <Modal  visible={props.isModalVisible} onOk={props.handleOk} onCancel={props.handleCancel} footer={null}>
-                <div className="signup-style-in-modal">
-                    <SignUpModal handleUserData={props.handleUser} 
-                    addUser={addUser} /> 
-                </div>
-            </Modal>
+        <div className="login-form">
+        <div className="login-information">
+        <input className="email-information" type={Text} placeholder="Email Adress or phone number"></input>
+        <input  className="password-information" type="password" placeholder="password"></input>
         </div>
-        </Aux>
+        <div className="login-button">
+        <button className="button-login" onClick={goToHomePage}>Login in</button>
+        </div>
+        <div className="a-tag"><Button>Forgotten Password?</Button></div>
+        <div className="line">
+        </div>
+        <button className="acc-button" onClick={showModal}>Create New Account</button>
+        <>
+      <Modal  visible={isModalVisible} onOk={handleOk} onCancel={handleCancel} footer={null}>
+      <SignUpModal handleUserData={props.handleUser} />
+      </Modal>
+    </>
+
+        </div>
     )
 }
 export default FreelancerLoginFrom;
--- src/components/homePage/homePage.js
+++ src/components/homePage/homePage.js
@@ -1,13 +1,13 @@
 import React from "react";
 import './homePage.css'
-import {Logo} from "./images/2X4thv_T_400x400.jpg";
+import Logo from "./images/2X4thv_T_400x400.jpg";
 function homePage() {
-<<<<<<< HEAD
+
   return (
     <div className="Profile">
       <div className="Profile-container">
         <div className="img-profile">
-          <img src="{Logo}" className="img-profile"></img>
+          <img src={Logo} className="img-profile"></img>
         </div>
         <div className="user-names">
           <p className="user-work">Front-end Developer</p>
@@ -19,11 +19,6 @@ function homePage() {
         </div>
         <div className="">
           <button>View More</button>
-=======
-    return (
-        <div>
-            
->>>>>>> 4a3a58a16d3b5c9a7f6c20b8bfc38bd3a6953d34
         </div>
       </div>
     </div>
